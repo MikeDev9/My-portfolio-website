da3f2b064986c77b566c9b9352500efedc4eb515
LearnVue201 <learnmore9723@gmail.com>
2021-09-28 16:36:22 +0700

    test

--- mail.php
+++ mail.php
@@ -1,17 +1,18 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$project= $_POST['project'];
-$message= $_POST['message'];
-$to = "devosec21@gmail.com";
-$subject = "Mail From mikedev9";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Project = " . $project . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
-?>
+
+ if (isset($_POST['submit'])) {
+$placeholder = $_POST[ 'name'];
+$placeholder = $_POST['email'];
+$placeholder = $_POST['project'];
+$placeholder = $_POST['message'];
+
+$emailTo = "devosec21@gmail.com";
+$headers = "From: ".$placeholder;
+&txt = " got email from" .$name.".\n\n".$message;
+
+
+$emailTo($placeholder, $placeholder, $placeholder, $placeholder);
+header("Location: index.php?emailsend")
+
+
+ }
