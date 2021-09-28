87ca42f922ee9836720223c1caa8a019bb7e4207
LearnVue201 <learnmore9723@gmail.com>
2021-09-28 17:09:01 +0700

    test

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
     // More headers
-    $headers .= 'From: bravecoderofficial@gmail.com';
+    $headers .= 'From: devosec21@gmail.com';
 
     $mail = mail($to,$subject,$message,$headers);
 
